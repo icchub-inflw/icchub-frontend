6eff60adf07df71261dc3e281fbd5cccf1e32d74
Val O <valerie.osei@gmail.com>
2025-06-12 20:18:44 -0400

    updated faqs

--- src/components/sections/faq-banner.tsx
+++ src/components/sections/faq-banner.tsx
@@ -11,6 +11,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const homeownerFaqs = [
   {
+    id: 1,
     question: "How exactly does the process work?",
     answer: [
       "Share your project details: describe your project as best as you can to inflw, our chatbot and your new best friend!",
@@ -20,12 +21,14 @@ const homeownerFaqs = [
     ],
   },
   {
+    id: 2,
     question: "How much does the process cost for homeowners?",
     answer: [
       "No cost to homeowners. trades pros pay a fee to be featured on the platform, in addition to a small transaction fee for processing each payment within the platform.",
     ],
   },
   {
+    id: 3,
     question: "Do I get multiple quotes – and if so, how many?",
     answer: [
       "inflw currently supports the pairing with trades pros and scheduling of the first encounters to get started right away. After the trades pros see your project, they will provide you with a quote.",
@@ -33,6 +36,7 @@ const homeownerFaqs = [
     ],
   },
   {
+    id: 4,
     question: "Why should I trust your trades pro network",
     answer: [
       "All trades pros within the platform have been vetted based on factors including, their standing in online consumer protection databases, adequate licensing and insurance, strong customer feedback and reviews, and fair pricing (per the market).",
@@ -40,18 +44,21 @@ const homeownerFaqs = [
     ],
   },
   {
+    id: 5,
     question: "Are all trades pros on here general trades pros/ foremen?",
     answer: [
       "inflw pulls in trades pros at the subtrades pro level. Subtrades pros are tradespersons who specialize in a particular area of work (e.g., plumbing) and are typically hired by a general trades pro, however at an additional fee (i.e., about 20-30% extra). Our system aims to help keep prices affordable by leveraging AI to create teams with the skill set needed to meet your project needs.",
     ],
   },
   {
+    id: 6,
     question: "How will the quality of the project be monitored?",
     answer: [
       "inflw will give you the option to request an inspector to review at the mid- and end- stages of the project.",
     ],
   },
   {
+    id: 7,
     question:
       "Can inflw provide recommendations for other professional services related to my home renovation (i.e., architects or interior designers)?",
     answer: [
@@ -59,12 +66,14 @@ const homeownerFaqs = [
     ],
   },
   {
+    id: 8,
     question: "What types of renovation projects do you do?",
     answer: [
       "Currently, we support kitchen, bathroom, living room and dining room related renovations, which may include but are not limited to plumbing, carpentry, or electrical work.",
     ],
   },
   {
+    id: 9,
     question:
       "Does inflw have a restriction around the size of projects they will take on?",
     answer: [
@@ -72,6 +81,7 @@ const homeownerFaqs = [
     ],
   },
   {
+    id: 10,
     question: "Are there ratings I can review?",
     answer: [
       "Yes! On each trades pro profile we include a synthesized summary of past reviews from past projects, highlighting the key themes that come out of the feedback from homeowners.",
@@ -88,18 +98,21 @@ const homeownerFaqs = [
 
 const contractorFaqs = [
   {
+    id: 11,
     question: "Who can use the platform",
     answer: [
       "Inflw pulls in trades pros at the subtrades pro level (i.e., tradespersons who specialize in a particular area of work [e.g., plumbing]). We then leverage AI to create teams with the skill sets needed to meet homeowner project needs.",
     ],
   },
   {
+    id: 12,
     question: "How do I get leads?",
     answer: [
       "We ask homeowners to describe their project as best as they can, asking additional prompts to ensure we get the preliminary detail you need! We then scan our trades pro network, to assess the availability and alignment of skill sets required for the job. Homeowners gets the opportunity to review your profile and decide on whether to schedule a meeting with you in person (no initial bids needed).",
     ],
   },
   {
+    id: 13,
     question: "How do I get paid?",
     answer: [
       "First encounter: homeowners will be required to pay for your initial site visit, a fee of which will be reduced from the total project cost to be paid.",
@@ -107,12 +120,14 @@ const contractorFaqs = [
     ],
   },
   {
+    id: 14,
     question: "How do I join the trades pro network",
     answer: [
       "We ask that you complete the trades pros’ applications through which your professional background will be assessed. Provided you meet all of the eligibility requirements you will be invited into the network!",
     ],
   },
   {
+    id: 15,
     question: "What are the fees for using the platform",
     answer: [
       "Monthly payment: trades pros are expected to pay a fee to be featured on the platform and paired with projects. Transaction fees: A small transaction fee will be made for each processed payment within the platform. Please note the transaction fees will be waived for your first project within the platform!",
@@ -147,7 +162,7 @@ export default function FAQAccordion() {
                 <AccordionContent>
                   <ul className="list-disc pl-6 space-y-2 text-gray-800 text-base">
                     {faq.answer.map((answer) => {
-                      return <li>{answer}</li>;
+                      return <li key={index}>{answer}</li>;
                     })}
                   </ul>
                 </AccordionContent>
@@ -169,7 +184,7 @@ export default function FAQAccordion() {
                   {" "}
                   <ul className="list-disc pl-6 space-y-2 text-gray-800 text-base">
                     {faq.answer.map((answer) => {
-                      return <li>{answer}</li>;
+                      return <li key={index}>{answer}</li>;
                     })}
                   </ul>
                 </AccordionContent>
